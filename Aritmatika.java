38e5a6cb4eabe2fb9781535fa76f34cfa63d7c04
richmond janus rafii aryanto <53498537+Richmondjanusrafiiaryanto@users.noreply.github.com>
2019-08-22 14:51:44 +0700

    Add files via upload

---
+++ Aritmatika.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aritmatika;
+
+/**
+ *
+ * @author surga 2019
+ */
+public class Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int a = 10;
+    int b = 2;
+    
+    int c;
+    
+    
+    //Penjumlahan
+    c = a + b;
+    System.out.println(a + " + " + b + " - " + c);
+    
+    //Pengurangan
+     c = a - b;
+    System.out.println(a + " - " + b + " = " + c);
+    
+    //Perkalian 
+    c = a * b;
+    System.out.println(a + " x " + b + " = " + c);
+    
+    //Pembagian
+     c = a / b;
+    System.out.println(a + " : " + b + " = " + c);
+    
+    //sisa pembagian
+     c = a % b;
+    System.out.println(a + " % " + b + " = " + c);
+    
+    
+           
+    }
+    
+}
